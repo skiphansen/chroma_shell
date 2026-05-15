dc5a31bce94176c499c3e03844bae12555f51330
Skip Hansen <skip@gfrn.org>
2026-05-15 12:07:54 -0700

    Added clear screen option to PDI 7.4" BWRY tests.

--- chroma_shell/E2741QS0B3/test_7_4_bwry.cpp
+++ chroma_shell/E2741QS0B3/test_7_4_bwry.cpp
@@ -36,15 +36,17 @@ extern "C" void EpdTestBWRY_7_4(char *CmdLine)
    Pervasive_BWRY_Medium epdtest(eScreen_EPD_741_QS_0B);
 
    do {
+      printf("PDI 7.4\" BWRY tests\n");
       int TestType = 0;
       sscanf(CmdLine,"%d",&TestType);
-      if(TestType <= 0 || TestType > 2) {
+      if(TestType <= 0 || TestType > 3) {
          if(TestType != 0) {
             printf("Invalid test type\n");
          }
          printf("select test type:\n");
-         printf("  1 - draw test pattern\n");
+         printf("  1 - draw bounding box\n");
          printf("  2 - display test image\n");
+         printf("  3 - clear screen\n");
          break;
       }
 
@@ -52,14 +54,13 @@ extern "C" void EpdTestBWRY_7_4(char *CmdLine)
          printf("displaying test image \n");
          epdtest.updateNormal(BW_monoBuffer, frameSize);
       }
-      else if(TestType == 1) {
+      else if(TestType == 1 || TestType == 3) {
          int y;
          if((Frame = (uint8_t *) malloc(frameSize)) == NULL) {
             printf("malloc failed\n");
             break;
          }
 
-         printf("drawing bounding box\n");
       // draw horizontal black line across the top of the display
          int xIncrement = 480 / 4;  // byte address increase for each line of y
 // 0x00 = all black
@@ -67,16 +68,22 @@ extern "C" void EpdTestBWRY_7_4(char *CmdLine)
 // 0xaa = all yellow
 // 0xff = all red
          memset(Frame,0x55,frameSize);
-      // draw horizontal black line across the top
-         memset(Frame,0,480/4);
-      // draw horizontal black line across the bottom of the display
-         int yOffset = xIncrement * 799;
-         memset(&Frame[yOffset],0,480/4);
+         if(TestType == 1) {
+            printf("drawing bounding box\n");
+         // draw horizontal black line across the top
+            memset(Frame,0,480/4);
+         // draw horizontal black line across the bottom of the display
+            int yOffset = xIncrement * 799;
+            memset(&Frame[yOffset],0,480/4);
 
-      // draw black line on right and left hand sides of the display
-         for(y = 0; y < 800; y++) {
-            Frame[xIncrement * y] &= 0x3f;
-            Frame[(xIncrement * y) + (479/4)] &= 0xfc;
+         // draw black line on right and left hand sides of the display
+            for(y = 0; y < 800; y++) {
+               Frame[xIncrement * y] &= 0x3f;
+               Frame[(xIncrement * y) + (479/4)] &= 0xfc;
+            }
+         }
+         else {
+            printf("clearing screen\n");
          }
          epdtest.updateNormal(Frame,frameSize);
       }
